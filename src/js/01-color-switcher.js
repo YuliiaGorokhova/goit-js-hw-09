0f61ce82c8bf83d1a54a3e2aefda399953e1556a
YuliiaGorokhova <Julia_gorokhova@ukr.net>
2022-12-07 21:28:25 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true; //блокуємо кнопку
+  stopBtn.disabled = false;
+
+  intervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startBtn.disabled = false; //зняття блоку з кнопки
+  stopBtn.disabled = true;
+});
