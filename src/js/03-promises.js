dd1c65c4124993fbd8e17e7f07b2a862bafb7771
YuliiaGorokhova <Julia_gorokhova@ukr.net>
2022-12-09 21:38:00 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('form');
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
+formEl.addEventListener('submit', onSubmitHandler);
+
+function onSubmitHandler(event) {
+  event.preventDefault();
+  let delayPromise = Number(delayEl.value);
+
+  for (let i = 1; i <= amountEl.value; i += 1) {
+    createPromise(i, delayPromise).then(onSuccsess).catch(onError);
+    delayPromise += Number(stepEl.value);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
+}
+
+function onSuccsess(result) {
+  Notiflix.Notify.success(result);
+}
+function onError(error) {
+  Notiflix.Notify.failure(error);
 }
